f7d794e913c8d5e1a94905558ecf6d7246999829
VisonaN <paulovisona@gmail.com>
2024-01-23 14:42:10 -0300

    início no TS

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function printObject(pessoa) {
+    console.log(pessoa);
+}
+printObject({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+});
+// Para executar o ts no node tem que colocar o comando no terminal: npx (node package exporter) e tsc (typescript compyler) e aí depois colcoar o src/index.ts para traduzir o que foi feito em TS para JS e aí sim pode executar o código
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+type hero={
+    name: string;
+    vulgo: string;
+};
+
+function printObjects(pessoa: hero) {
+    console.log(pessoa);
+}
+
+printObjects({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+});
+
+// Para executar o ts no node tem que colocar o comando no terminal: npx (node package exporter) e tsc (typescript compyler) e aí depois colcoar o src/index.ts para traduzir o que foi feito em TS para JS e aí sim pode executar o código
