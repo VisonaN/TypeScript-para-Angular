7ffd3d82f2230a60a0ed96dffa97ac6cf1fe4c40
VisonaN <paulovisona@gmail.com>
2024-01-24 10:38:40 -0300

    aaaaa

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/*
 type hero={
     name: string;
     vulgo: string;
@@ -13,3 +14,26 @@ printObjects({
 });
 
 // Para executar o ts no node tem que colocar o comando no terminal: npx (node package exporter) e tsc (typescript compyler) e aí depois colcoar o src/index.ts para traduzir o que foi feito em TS para JS e aí sim pode executar o código
+*/
+
+/**
+ * Variáveis
+ */
+
+// TIPOS DE DADOS PRIMITIVOS: boolean, number, string
+    let ligado:boolean = true; // Dessa forma a variavel só aceita o tipo colocado antes de atribuir o valor
+//              -> colocando o tipo da variável
+// Tem outra maneira que vai ser tipada por inferência, que é quando não declara o tipo da variável antes de atribuir valor nela:
+    let ligadu = false;
+    let nome:string = "Paulo Visoná";
+    let idade:number = 27;
+    let altura:number = 1.65; // Pode colocar números decimais 
+
+// TIPOS ESPECIAIS: null e undefined
+    let nulo:null = null;
+    // Nesse caso o tipo null só pode receber o valor null mesmo
+    let indefinido:undefined = undefined; // Mesma coisa da null
+
+// TIPOS ABRANGENTES: any e void
+    let retorno:void // Usa o tipo void quando realmente não é pra retornar nada // Por ser vazio não tem que adicionar valor dentro
+    let retornoView:any = "Aceita qualquer coisa"; // Utilizar :any quando você tem uma função que pode retornar algo não previsível, pode retornar um objeto, void, string...
