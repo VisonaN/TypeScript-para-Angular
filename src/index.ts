c5748dec4916e37fadccdf369a552fe2387a9101
VisonaN <paulovisona@gmail.com>
2024-01-24 14:06:08 -0300

    mais TS

--- src/index.ts
+++ src/index.ts
@@ -37,3 +37,34 @@ printObjects({
 // TIPOS ABRANGENTES: any e void
     let retorno:void // Usa o tipo void quando realmente não é pra retornar nada // Por ser vazio não tem que adicionar valor dentro
     let retornoView:any = "Aceita qualquer coisa"; // Utilizar :any quando você tem uma função que pode retornar algo não previsível, pode retornar um objeto, void, string...
+
+/* Objeto -> uma variável definida como objeto vai aceitar qualquer valor desde que esteja dentro de um objeto, ou seja, só vai aceitar um objeto como resposta, independente do que tem dentro dele. (sem previsibilidade)
+    let produto:object = {
+        name: "Paulo",
+        cidade: "jf",
+        idade: 27
+    };
+*/
+// Definindo uma estrutura para um objeto tipado - com previsibilidade ->
+type ProdutoLoja = {
+    name:string;
+    price:number;
+    unities:number;
+};
+
+let meuProduto: ProdutoLoja = {
+    name: "Tênis",
+    price: 90.91,
+    unities: 12
+};
+
+// Arrays podem ser declarados de duas formas:
+// 1
+let dados: string[] = ["Paulo", "Mylla", "Minie"];
+// 2
+let dados2: Array<string> = ["Paulo", "Mylla", "Minie"];
+
+// No TS você pode declarar arrays de dois tipos (ou multi tipos)
+
+let infos: (string | number)[] = ["Paulo", 27, "Mylla", 24];
+// Mas é melhor criar um objeto tipado para receber mais de um tipo para não correr risco de dar algum erro
